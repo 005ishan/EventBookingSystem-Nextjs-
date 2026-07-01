fa36459f96b14037aa85efa04128aac5619cc8c3
0050505ishan <isan91798@gmail.com>
2026-07-01 10:48:00 +0545

    create Skeleton component with shimmer animation

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import Skeleton from "@/components/Skeleton";
+
+export { SkeletonEventCard, SkeletonEventCardGrid, SkeletonProfile, SkeletonText, SkeletonAvatar, SkeletonRect } from "@/components/Skeleton";
+
+export default Skeleton;
+
